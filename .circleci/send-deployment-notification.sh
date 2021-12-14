f3a1813787d40f0a3f4d11347317b9c94a0402ec
hisomura <hisomura.noreply@gmail.com>
2021-12-14 11:25:59 +0900

    arrange

--- .circleci/send-deployment-notification.sh
+++ .circleci/send-deployment-notification.sh
@@ -8,8 +8,9 @@
 #   0 if thing goes well, non-zero on error.
 #######################################
 function send_notification_to_slack() {
+  echo "text: $1"
   ## https://swfz.hatenablog.com/entry/2019/07/07/035341
-  curl -X POST ${SLACK_WEBHOOK_URL} \
+  curl -X POST "$SLACK_WEBHOOK_URL" \
     -H "Content-Type: application/json" \
     -d @- <<EOS
 {
@@ -21,16 +22,11 @@ EOS
   return $?
 }
 
-declare -A MEMBERS_MAP=(
-  ["hisomura"]="U01MG8FKHFE"
-)
-
 ### Main part
-member_id=$(echo $MEMBERS_MAP_JSON | jq -r ."$CIRCLE_USERNAME")
-echo $member_id
+member_id=$(echo "$MEMBERS_MAP_JSON" | jq -r ."$CIRCLE_USERNAME")
 
 mention=''
-if [ $member_id != 'null' ]; then
+if [ "$member_id" != 'null' ]; then
   mention="<@${member_id}>"
 fi
 send_notification_to_slack "$mention test"

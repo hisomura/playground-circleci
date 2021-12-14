f38e8ef8616de1638fc637cf13cb2ae19b9cb3ad
hisomura <hisomura.noreply@gmail.com>
2021-12-14 11:35:56 +0900

    test2

--- .circleci/send-deployment-notification.sh
+++ .circleci/send-deployment-notification.sh
@@ -2,10 +2,12 @@
 
 #######################################
 # Send a notification to Commmune slack workspace.
+# Globals:
+#   SLACK_WEBHOOK_URL
 # Arguments:
 #   Text to send
 # Returns:
-#   0 if thing goes well, non-zero on error.
+#   0 if a message is sent, non-zero on error.
 #######################################
 function send_notification_to_slack() {
   echo "text: $1"
@@ -23,10 +25,12 @@ EOS
 }
 
 ### Main part
-member_id=$(echo "$MEMBERS_MAP_JSON" | jq -r ."$CIRCLE_USERNAME")
+### Main part
+if [ $CIRCLE_BRANCH == 'main' ]; then
+  member_id=$(echo "$MEMBERS_MAP_JSON" | jq -r ."$CIRCLE_USERNAME")
+  if [ "$member_id" != 'null' ]; then
+    mention="<@${member_id}>"
+  fi
 
-mention=''
-if [ "$member_id" != 'null' ]; then
-  mention="<@${member_id}>"
+  send_notification_to_slack "$mention develop環境にデプロイしました。 ビルド詳細: ${CIRCLE_BUILD_URL} プルリクエストURL: ${CIRCLE_PULL_REQUEST}"
 fi
-send_notification_to_slack "$mention test"
